eaa2fd859b513371e59527414989f41e7b285f0f
vantuan179 <tuan.phanvan179@gmail.com>
2024-07-30 15:37:14 +0700

    init

---
+++ src/main/java/com/zuno/dayoff/DayOffManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zuno.dayoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayOffManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayOffManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuno/dayoff/DayOffManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuno.dayoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayOffManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
